8dd70a6eca92f819e733eeb44f96a43250ec0a23
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-02-24 08:55:51 +0100

    internationalization, extension installable

--- setup.py
+++ setup.py
@@ -17,14 +17,32 @@ setup(
     license='',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.dataset_comments'],
+    package_data={'': [
+        'i18n/*/LC_MESSAGES/*.po',
+        'templates/*.html',\
+        'templates/admin/*.html',\
+        'templates/comments/*.html',\
+        'templates/package/*.html',\
+        'templates/package/snippets/*.html',\
+        'templates/related/*.html',\
+        'templates/related/snippets/*.html',\
+        'public/*.css']},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
     ],
-    entry_points='''
-        [ckan.plugins]
-        # Add plugins here, e.g.
-        dataset_comments=ckanext.dataset_comments.plugin:DatasetCommentsPlugin
-    ''',
+    #entry_points='''
+    #    [ckan.plugins]
+    #    # Add plugins here, e.g.
+    #    dataset_comments=ckanext.dataset_comments.plugin:DatasetCommentsPlugin
+    #''',
+    entry_points={
+        'babel.extractors': [
+                    'ckan = ckan.lib.extract:extract_ckan',
+                    ],
+        'ckan.plugins' : [
+                    'dataset_comments = ckanext.dataset_comments.plugin:DatasetCommentsPlugin',
+                    ]
+        }
 )
